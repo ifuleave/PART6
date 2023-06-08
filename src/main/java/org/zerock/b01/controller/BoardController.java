59879215601b0b60908c23debd0809139523cea1
sujin <ifuleav_e@naver.com>
2023-06-08 20:08:10 +0900

    하는데 까지 commit 게시물 수정

--- src/main/java/org/zerock/b01/controller/BoardController.java
+++ src/main/java/org/zerock/b01/controller/BoardController.java
@@ -3,6 +3,9 @@ package org.zerock.b01.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -14,12 +17,18 @@ import org.zerock.b01.dto.*;
 import org.zerock.b01.service.BoardService;
 
 import javax.validation.Valid;
+import java.io.File;
+import java.nio.file.Files;
+import java.util.List;
 
 @Controller
 @RequestMapping("/board")
 @Log4j2
 @RequiredArgsConstructor
 public class BoardController {
+
+    @Value("${org.zerock.upload.path}")// import 시에 springframework으로 시작하는 Value
+    private String uploadPath;
 
     private final BoardService boardService;
 
@@ -129,17 +138,44 @@ public class BoardController {
 
 
     @PostMapping("/remove")
-    public String remove(Long bno, RedirectAttributes redirectAttributes) {
+    public String remove(BoardDTO boardDTO, RedirectAttributes redirectAttributes) {
 
+        Long bno = boardDTO.getBno();
         log.info("remove post.. " + bno);
 
         boardService.remove(bno);
 
+        // 게시물이 데이터베이스상에서 삭제되었다면 첨부파일 삭제
+        log.info(boardDTO.getFileNames());
+        List<String> fileNames = boardDTO.getFileNames();
+        if (fileNames != null && fileNames.size() > 0) {
+            removeFiles(fileNames);
+        }
         redirectAttributes.addFlashAttribute("result", "removed");
 
         return "redirect:/board/list";
 
     }
+
+    public void removeFiles(List<String> files) {
+        for (String fileName : files) {
+            Resource resource = new FileSystemResource(uploadPath + File.separator + fileName);
+
+            String resourceName = resource.getFilename();
+
+            try {
+                String contentType = Files.probeContentType(resource.getFile().toPath());
+                resource.getFile().delete();
+                // 섬네일이 존재한다면
+                if (contentType.startsWith("image")) {
+                    File thumbnailFile = new File(uploadPath + File.separator + "s_" + fileName);
+                    thumbnailFile.delete();
+                }
+            }catch (Exception e){
+                log.error(e.getMessage());
+            }
+        }
+    }// end for
 
 
 }
